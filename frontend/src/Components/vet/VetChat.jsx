88d3037b7b8a3678a8dfdf674b9165b5a59c6e34
Nikhil Bhattarai <166083159+Nikhil78945621@users.noreply.github.com>
2025-05-11 08:41:53 +0545

    Update VetChat.jsx

--- frontend/src/Components/vet/VetChat.jsx
+++ frontend/src/Components/vet/VetChat.jsx
@@ -20,7 +20,7 @@ const VetChat = () => {
     }
     try {
       const response = await axios.get(
-        "http://localhost:8084/api/home-visits/vet",
+        "https://animalaid-9duz.onrender.com/api/home-visits/vet",
         {
           headers: { Authorization: `Bearer ${token}` },
         }
@@ -60,7 +60,7 @@ const VetChat = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:8084/api/home-visits/send-message",
+        "https://animalaid-9duz.onrender.com/api/home-visits/send-message",
         {
           requestId,
           message: `Vet: ${newMessage}`,
